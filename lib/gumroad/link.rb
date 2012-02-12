c32ef97097e0c20d7a1743f820c7c85fc531aedf
maxstoller <maxstoller@gmail.com>
2012-02-12 11:29:17 -0500

    added id to link object

--- lib/gumroad/link.rb
+++ lib/gumroad/link.rb
@@ -1,11 +1,11 @@
 module Gumroad
   class Link
     attr_reader :json
-    attr_accessor :name, :url, :description, :user, :price_cents 
+    attr_accessor :id, :name, :url, :description, :user, :price_cents 
     
     def initialize(session, json)
       @session = session
-      [:name, :url, :description, :user, :price_cents].each do |attribute|
+      [:id, :name, :url, :description, :user, :price_cents].each do |attribute|
         instance_variable_set(:"@#{attribute}", json[attribute.to_s])
       end
     end
